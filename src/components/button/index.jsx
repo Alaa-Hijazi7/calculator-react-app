950156997baf5cbf9c011e1eab6854d82c58bcbf
Alaa-Hijazi7 <alaa.hijayyad@gmail.com>
2021-02-22 13:55:39 +0200

    change class name

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./button.css";
 
 const Button = ({content, type,onButtonClick}) => {
-    return <div className={`Button ${content === "0" ? "Zero" : ""}
+    return <div className={`button ${content === "0" ? "Zero" : ""}
     ${type || ""}`} onClick={onButtonClick(content)}> {content}</div >;
 }
 export default Button;
