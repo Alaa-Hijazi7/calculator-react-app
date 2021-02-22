620cae00443e2925343b6ac7c6b4327e03e29953
Alaa-Hijazi7 <alaa.hijayyad@gmail.com>
2021-02-22 13:55:53 +0200

    fix index page

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,4 +1,4 @@
-import React,{ useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Button from "../button";
 
@@ -29,11 +29,11 @@ const App = () => {
         }
         if (content === ".") {
             if (val.includes(".")) return;
-            
+
             setVal(val + ".");
             return;
         }
-        
+
         if (content === "+") {
             if (operator != null) {
                 if (operator === "+") {
@@ -87,9 +87,10 @@ const App = () => {
             }
             setVal("0");
             setOperator("×");
-            return;        }
+            return;
+        }
         if (content === "÷") {
-            
+
             if (operator != null) {
                 if (operator === "÷") {
                     setFirstNum(firstNum + parseFloat(val));
@@ -104,7 +105,7 @@ const App = () => {
                 setFirstNum(parseFloat(val));
             }
 
-            
+
 
             setVal("0");
             setOperator("÷");
@@ -114,7 +115,7 @@ const App = () => {
 
             if (!operator) {
                 return;
-            } 
+            }
             if (operator === "+") {
                 setVal((firstNum + parseFloat(val)).toString());
             } else if (operator === "-") {
@@ -136,18 +137,18 @@ const App = () => {
         }
 
 
-            
-    };    
+
+    };
     window.addEventListener("keydown", (event) => {
         if (event.key === "1") {
             setVal("1");
         }
     })
-     window.addEventListener("keydown", (event) => {
+    window.addEventListener("keydown", (event) => {
         if (event.key === "2") {
             setVal("2");
         }
-     })
+    })
     window.addEventListener("keydown", (event) => {
         if (event.key === "3") {
             setVal("3");
@@ -157,17 +158,17 @@ const App = () => {
         if (event.key === "4") {
             setVal("4");
         }
-        })
+    })
     window.addEventListener("keydown", (event) => {
         if (event.key === "5") {
             setVal("5");
         }
-        })
+    })
     window.addEventListener("keydown", (event) => {
         if (event.key === "6") {
             setVal("6");
         }
-        })
+    })
     window.addEventListener("keydown", (event) => {
         if (event.key === "7") {
             setVal("7");
@@ -178,40 +179,40 @@ const App = () => {
             setVal("8");
         }
     })
-     window.addEventListener("keydown", (event) => {
+    window.addEventListener("keydown", (event) => {
         if (event.key === "9") {
             setVal("9");
         }
-     })
-     window.addEventListener("keydown", (event) => {
+    })
+    window.addEventListener("keydown", (event) => {
         if (event.key === "0") {
             setVal("0");
         }
-      })
+    })
     return (
         <div className="App">
             <div className="display">{val}</div>
-                <div className="buttons" >
-                    <Button onButtonClick={handelBtn} content="AC" type="function"/>
-                    <Button onButtonClick={handelBtn} content="+/-" type="function"/>
-                    <Button onButtonClick={handelBtn} content="%" type="function"/>
-                    <Button onButtonClick={handelBtn} content="÷" type="operator"/>
-                    <Button onButtonClick={handelBtn} content="7"/>
-                    <Button onButtonClick={handelBtn} content="8"/>
-                    <Button onButtonClick={handelBtn} content="9"/>
-                    <Button onButtonClick={handelBtn} content="×" type="operator"/>
-                    <Button onButtonClick={handelBtn} content="4"/>
-                    <Button onButtonClick={handelBtn} content="5"/>
-                    <Button onButtonClick={handelBtn} content="6"/>
-                    <Button onButtonClick={handelBtn} content="-" type="operator"/>
-                    <Button onButtonClick={handelBtn} content="1"/>
-                    <Button onButtonClick={handelBtn} content="2"/>
-                    <Button onButtonClick={handelBtn} content="3"/>
-                    <Button onButtonClick={handelBtn} content="+" type="operator"/>
-                    <Button onButtonClick={handelBtn} content="0"/>
-                    <Button onButtonClick={handelBtn} content="."/>
-                    <Button onButtonClick={handelBtn} content="=" type="operator"/>
-                </div>
+            <div className="buttons" >
+                <Button onButtonClick={handelBtn} content="AC" type="function" />
+                <Button onButtonClick={handelBtn} content="+/-" type="function" />
+                <Button onButtonClick={handelBtn} content="%" type="function" />
+                <Button onButtonClick={handelBtn} content="÷" type="operator" />
+                <Button onButtonClick={handelBtn} content="7" />
+                <Button onButtonClick={handelBtn} content="8" />
+                <Button onButtonClick={handelBtn} content="9" />
+                <Button onButtonClick={handelBtn} content="×" type="operator" />
+                <Button onButtonClick={handelBtn} content="4" />
+                <Button onButtonClick={handelBtn} content="5" />
+                <Button onButtonClick={handelBtn} content="6" />
+                <Button onButtonClick={handelBtn} content="-" type="operator" />
+                <Button onButtonClick={handelBtn} content="1" />
+                <Button onButtonClick={handelBtn} content="2" />
+                <Button onButtonClick={handelBtn} content="3" />
+                <Button onButtonClick={handelBtn} content="+" type="operator" />
+                <Button onButtonClick={handelBtn} content="0" />
+                <Button onButtonClick={handelBtn} content="." />
+                <Button onButtonClick={handelBtn} content="=" type="operator" />
+            </div>
         </div>
     )
 }
